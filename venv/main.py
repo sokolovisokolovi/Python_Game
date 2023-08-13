25fdae44c2f06f558f78405a118b345f8af8ed1a
sokolovisokolovi <sokolovisokolovi@yandex.ru>
2023-08-13 22:54:19 +0300

    Less #5

--- venv/main.py
+++ venv/main.py
@@ -1,16 +1,27 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((600, 300))        # flags=pygame.NOFRAME
+screen = pygame.display.set_mode((1080, 920))        # flags=pygame.NOFRAME
 pygame.display.set_caption("Pygame igra")
 icon = pygame.image.load("icon/icon.png")
 pygame.display.set_icon(icon)
 
+square = pygame.Surface((50, 170))
+square.fill('Blue')
+
+myfont = pygame.font.Font('fonts/fonts.ttf', 35)
+text_surface = myfont.render('itProger', True, 'Red')
+
+player = pygame.image.load("icon/icon.png")
 
 running = True
 while True:
 
-    # screen.fill((219, 218, 213, 255))
+    pygame.draw.circle(square, 'Red', (10, 5), 20)
+
+    screen.blit(square, (100, 0))
+    screen.blit(text_surface, (300, 100))
+    screen.blit(player, (100, 50))
 
     pygame.display.update()
 
@@ -18,6 +29,3 @@ while True:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_1:
-                screen.fill((190, 38, 170))
